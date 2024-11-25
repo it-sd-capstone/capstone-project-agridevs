aeb5a67cc11b792be47e84ebe8770ff9c7cb08f2
gsarauer4 <gsarauer4@student.cvtc.edu>
2024-11-25 15:56:22 -0600

    Fix App.js for UploadComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import React from 'react';
 import TestComponent from './TestComponent';
-import CSVUploadComponent from "./CSVUploadComponent";
+import CSVUploadComponent from './CSVUploadComponent';
 
 function App() {
-  return (
-      <div className="App">
-        <h1>Welcome to Profit Map Web App</h1>
-        <TestComponent/>
-        <CSVUploadComponent/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Profit Map Web App</h1>
+            </header>
+            <div>
+                <TestComponent />
+            </div>
+            <div>
+                <CSVUploadComponent />
+            </div>
+        </div>
+    );
 }
 
 export default App;
