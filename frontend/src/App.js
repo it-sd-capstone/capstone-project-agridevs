051f36cdd2a19eae562de709b947dd94f9fad780
gsarauer4 <gsarauer4@student.cvtc.edu>
2024-11-25 21:08:42 -0600

    Comment out app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,41 @@
-import React, {useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home'; // Make sure the import is correct
-import CreateAccountPage from './CreateAccountPage'; // Correct the path and component name
-import './Home.css';
-import TestComponent from './TestComponent';
-import CSVUploadComponent from './CSVUploadComponent';
+// import React, {useEffect} from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './Home'; // Make sure the import is correct
+// import CreateAccountPage from './CreateAccountPage'; // Correct the path and component name
+// import './Home.css';
+// import TestComponent from './TestComponent';
+// import CSVUploadComponent from './CSVUploadComponent';
 
-function App() {
+// function App() {
 
-    useEffect(() => {
+//     useEffect(() => {
         // Redirect to /Home when app loads
-        window.location.href = '/home';
-    }, []);
+//         window.location.href = '/home';
+//     }, []);
 
-    return (
-        <Router>
-            <Routes>
-                {/* Route to Home Component */}
-                <Route path="/Home" element={<Home />} />
-                {/* Route to Create Account Component */}
-                <Route path="/CreateAccountPage" element={<CreateAccountPage />} />
-            </Routes>
-        </Router>
-    );
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Welcome to Profit Map Web App</h1>
-            </header>
-            <div>
-                <TestComponent />
-            </div>
-            <div>
-                <CSVUploadComponent />
-            </div>
-        </div>
-    );
-}
+//     return (
+//         <Router>
+//             <Routes>
+//                 {/* Route to Home Component */}
+//                 <Route path="/Home" element={<Home />} />
+//                 {/* Route to Create Account Component */}
+//                 <Route path="/CreateAccountPage" element={<CreateAccountPage />} />
+//             </Routes>
+//         </Router>
+//     );
+//     return (
+//        <div className="App">
+//             <header className="App-header">
+//                 <h1>Welcome to Profit Map Web App</h1>
+//             </header>
+//             <div>
+//                 <TestComponent />
+//             </div>
+//             <div>
+//                 <CSVUploadComponent />
+//             </div>
+//         </div>
+//     );
+// }
 
-export default App;
+// export default App;
