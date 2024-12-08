827e20784ebee093ad48822a3ed0d9f895d818fb
gsarauer4 <gsarauer4@student.cvtc.edu>
2024-12-07 20:58:05 -0600

    Rebuild canvas

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL = 'https://capstone-project-agridevs.onrender.com';
-export const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+// export const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
